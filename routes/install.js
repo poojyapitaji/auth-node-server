9f09ab6b2469dd3d81f5762bc31bd98fb4912f53
Shubham Singh <shubhams.28@yahoo.com>
2023-09-04 02:41:56 +0530

    removed un-used file

--- routes/install.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/install');
-
-router.post('/checkdb', controller.checkDbConnection);
-
-
-module.exports = router
